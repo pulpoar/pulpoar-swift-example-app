27eae269a88a9bd6505fe1c8914876a38bc14de6
erenkayacan <eren.kayacan1@gmail.com>
2022-05-26 14:24:57 +0300

    initial commit

---
+++ pulpoar-swift-example-app/EngineViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EngineViewController.swift
+//  pulpoar-swift-example-app
+//
+//  Created by Eren Kayacan on 24.05.2022.
+//
+
+import UIKit
+import WebKit
+
+class EngineViewController: UIViewController, WKScriptMessageHandler {
+    @IBOutlet private weak var keyEvent: UILabel!
+    @IBOutlet private weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let myURL = URL(string:"https://engine.pulpoar.com/engine/v0/ca8e71e3-58f0-40d9-b8e9-af0df5d2864b")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+        let contentController = self.webView.configuration.userContentController
+        contentController.add(self, name: "jsHandler")
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            guard let dict = message.body as? [String : AnyObject] else {
+                return
+            }
+        keyEvent.text = dict["message"] as? String
+        }
+}
---
+++ pulpoar-swift-example-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pulpoar-swift-example-app
+//
+//  Created by Eren Kayacan on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
