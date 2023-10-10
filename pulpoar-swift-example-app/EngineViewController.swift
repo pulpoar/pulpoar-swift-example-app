c105e59c7e7e1c3dc6581187981b049a5a0193c5
atknclk <80201441+atknclk@users.noreply.github.com>
2023-10-10 13:30:44 +0300

    Inline camera bug fix for realtime

--- pulpoar-swift-example-app/EngineViewController.swift
+++ pulpoar-swift-example-app/EngineViewController.swift
@@ -15,10 +15,17 @@ class EngineViewController: UIViewController, WKScriptMessageHandler {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let myURL = URL(string:"https://engine.pulpoar.com/engine/v0/ca8e71e3-58f0-40d9-b8e9-af0df5d2864b")
+        let configuration = WKWebViewConfiguration()
+        configuration.allowsInlineMediaPlayback = true
+
+        let webView = WKWebView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height), configuration: configuration)
+
+        view.addSubview(webView)
+
+        let myURL = URL(string: "https://engine.pulpoar.com/engine/v0/ca8e71e3-58f0-40d9-b8e9-af0df5d2864b")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
-        
+
         let contentController = self.webView.configuration.userContentController
         contentController.add(self, name: "jsHandler")
     }
